519179eeb4e43d8bd9de071a435dab015d7045ca
den <den.velez@gmail.com>
2024-04-25 13:16:01 -0600

    OD-15 signup page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,14 @@ import Link from "next/link";
 
 const HomePage = () => {
   return (
-    <section className='min-h-screen bg-white dark:bg-gray-900'>
-      <Link href='/login'> Iniciar Sesion</Link>
-      <div className='text-white flex flex-col items-center'>
-        <h1 className='text-h1'>header 1 PLpghq</h1>
-        <h2 className='text-h2'>header 2</h2>
-        <h3 className='text-h3'>header 3</h3>
-        <h4 className='text-h4'>header 4</h4>
-        <h5 className='text-h5'>header 5</h5>
-        <p>body</p>
-        <p>small</p>
-        <p>caption</p>
+    <section className='py-40 min-h-screen bg-white dark:bg-gray-900'>
+      <div className='text-white flex flex-col gap-10 items-center'>
+        <h1 className='text-h1'>
+          <Link href='/auth/login'> Ingresar</Link>
+        </h1>
+        <h1 className='text-h1'>
+          <Link href='/auth/signup'> Crear cuenta</Link>
+        </h1>
       </div>
     </section>
   );
