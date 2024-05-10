a21f678aa504fe39fe162f22c9b4485540849768
den <den.velez@gmail.com>
2024-05-10 13:59:00 -0600

    OD-25 fix odontogram size

--- src/app/patients/[id]/odontogram/page.tsx
+++ src/app/patients/[id]/odontogram/page.tsx
@@ -50,49 +50,49 @@ const odontogramUpItems = [
     image: "/images/odontogram/s8.png",
     statusStyles: STATUS_STYLES.ausente,
     styles:
-      "absolute w-12 h-10 right-[calc(50%+140px)] top-[210px] rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute w-12 h-10 right-[calc(50%+110px)] top-[210px] rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "17",
     image: "/images/odontogram/s7.png",
     statusStyles: STATUS_STYLES.corona,
     styles:
-      "absolute w-12 h-10 right-[calc(50%+130px)] top-[170px] rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute w-12 h-10 right-[calc(50%+105px)] top-[170px] rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "16",
     image: "/images/odontogram/s6.png",
     statusStyles: STATUS_STYLES.obturacion,
     styles:
-      "absolute w-12 h-10 right-[calc(50%+120px)] top-[130px] rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute w-12 h-10 right-[calc(50%+100px)] top-[130px] rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "15",
     image: "/images/odontogram/s5.png",
     statusStyles: STATUS_STYLES.cariesOclusal,
     styles:
-      "absolute w-11 h-10 right-[calc(50%+110px)] top-[95px] rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute w-11 h-10 right-[calc(50%+95px)] top-[95px] rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "14",
     image: "/images/odontogram/s4.png",
     statusStyles: STATUS_STYLES.cariesMesial.upper,
     styles:
-      "absolute w-11 h-10 right-[calc(50%+96px)] top-[60px]  rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute w-11 h-10 right-[calc(50%+85px)] top-[60px]  rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "13",
     image: "/images/odontogram/s3.png",
     statusStyles: STATUS_STYLES.cariesDistal.upper,
     styles:
-      "absolute w-10 h-10 right-[calc(50%+75px)] top-[30px]  rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute w-10 h-10 right-[calc(50%+65px)] top-[30px]  rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "12",
     image: "/images/odontogram/s2.png",
     statusStyles: STATUS_STYLES.cariesLingual.upper,
     styles:
-      "absolute w-10 h-10  right-[calc(50%+40px)] top-[10px]   rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute w-10 h-10  right-[calc(50%+35px)] top-[10px]   rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "11",
@@ -113,49 +113,49 @@ const odontogramUpItems = [
     image: "/images/odontogram/s2.png",
     statusStyles: STATUS_STYLES.cariesLingual.upper,
     styles:
-      "absolute -scale-x-[1] w-10 h-10  left-[calc(50%+40px)] top-[10px]   rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute -scale-x-[1] w-10 h-10  left-[calc(50%+35px)] top-[10px]   rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "23",
     image: "/images/odontogram/s3.png",
     statusStyles: STATUS_STYLES.cariesDistal.upper,
     styles:
-      "absolute -scale-x-[1] w-10 h-10 left-[calc(50%+75px)] top-[30px]  rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute -scale-x-[1] w-10 h-10 left-[calc(50%+65px)] top-[30px]  rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "24",
     image: "/images/odontogram/s4.png",
     statusStyles: STATUS_STYLES.cariesMesial.upper,
     styles:
-      "absolute -scale-x-[1] w-11 h-10 left-[calc(50%+96px)] top-[60px]  rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute -scale-x-[1] w-11 h-10 left-[calc(50%+85px)] top-[60px]  rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "25",
     image: "/images/odontogram/s5.png",
     statusStyles: STATUS_STYLES.cariesOclusal,
     styles:
-      "absolute -scale-x-[1] w-11 h-10 left-[calc(50%+110px)] top-[95px] rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute -scale-x-[1] w-11 h-10 left-[calc(50%+95px)] top-[95px] rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "26",
     image: "/images/odontogram/s6.png",
     statusStyles: STATUS_STYLES.obturacion,
     styles:
-      "absolute -scale-x-[1] w-12 h-10 left-[calc(50%+120px)] top-[130px] rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute -scale-x-[1] w-12 h-10 left-[calc(50%+100px)] top-[130px] rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "27",
     image: "/images/odontogram/s7.png",
     statusStyles: STATUS_STYLES.corona,
     styles:
-      "absolute -scale-x-[1] w-12 h-10 left-[calc(50%+130px)] top-[170px] rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute -scale-x-[1] w-12 h-10 left-[calc(50%+105px)] top-[170px] rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "28",
     image: "/images/odontogram/s8.png",
     statusStyles: STATUS_STYLES.ausente,
     styles:
-      "absolute -scale-x-[1] w-12 h-10 left-[calc(50%+140px)] top-[210px] rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute -scale-x-[1] w-12 h-10 left-[calc(50%+110px)] top-[210px] rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
 ];
 
@@ -165,42 +165,42 @@ const odontogramDownItems = [
     image: "/images/odontogram/i8.png",
     statusStyles: STATUS_STYLES.ausente,
     styles:
-      "absolute w-12 h-10 right-[calc(50%+130px)] bottom-[230px] rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute w-12 h-10 right-[calc(50%+105px)] bottom-[230px] rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "47",
     image: "/images/odontogram/i7.png",
     statusStyles: STATUS_STYLES.corona,
     styles:
-      "absolute w-12 h-10 right-[calc(50%+120px)] bottom-[185px] rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute w-12 h-10 right-[calc(50%+100px)] bottom-[185px] rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "46",
     image: "/images/odontogram/i6.png",
     statusStyles: STATUS_STYLES.obturacion,
     styles:
-      "absolute w-12 h-10 right-[calc(50%+110px)] bottom-[140px] rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute w-12 h-10 right-[calc(50%+95px)] bottom-[140px] rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "45",
     image: "/images/odontogram/i5.png",
     statusStyles: STATUS_STYLES.cariesOclusal,
     styles:
-      "absolute w-11 h-10 right-[calc(50%+100px)] bottom-[95px] rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute w-11 h-10 right-[calc(50%+90px)] bottom-[95px] rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "44",
     image: "/images/odontogram/i4.png",
     statusStyles: STATUS_STYLES.cariesMesial.lower,
     styles:
-      "absolute w-11 h-10 right-[calc(50%+86px)] bottom-[60px]  rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute w-11 h-10 right-[calc(50%+75px)] bottom-[60px]  rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "43",
     image: "/images/odontogram/i3.png",
     statusStyles: STATUS_STYLES.cariesDistal.lower,
     styles:
-      "absolute w-10 h-10 right-[calc(50%+65px)] bottom-[30px]  rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute w-9 h-10 right-[calc(50%+60px)] bottom-[30px]  rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "42",
@@ -214,14 +214,14 @@ const odontogramDownItems = [
     image: "/images/odontogram/i1.png",
     statusStyles: STATUS_STYLES.cariesVestibular.lower,
     styles:
-      "absolute w-9 h-10 right-[50%]  rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute w-8 h-10 right-[50%]  rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "31",
     image: "/images/odontogram/i1.png",
     statusStyles: STATUS_STYLES.cariesVestibular.lower,
     styles:
-      "absolute -scale-x-[1] w-9 h-10 left-[50%]  rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute -scale-x-[1] w-8 h-10 left-[50%]  rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "32",
@@ -235,7 +235,7 @@ const odontogramDownItems = [
     image: "/images/odontogram/i3.png",
     statusStyles: STATUS_STYLES.cariesDistal.lower,
     styles:
-      "absolute -scale-x-[1] w-10 h-10 left-[calc(50%+65px)] bottom-[30px]  rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute -scale-x-[1] w-9 h-10 left-[calc(50%+65px)] bottom-[30px]  rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "34",
@@ -249,28 +249,28 @@ const odontogramDownItems = [
     image: "/images/odontogram/i5.png",
     statusStyles: STATUS_STYLES.cariesOclusal,
     styles:
-      "absolute -scale-x-[1] w-11 h-10 left-[calc(50%+100px)] bottom-[95px] rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute -scale-x-[1] w-11 h-10 left-[calc(50%+90px)] bottom-[95px] rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "36",
     image: "/images/odontogram/i6.png",
     statusStyles: STATUS_STYLES.obturacion,
     styles:
-      "absolute -scale-x-[1] w-12 h-10 left-[calc(50%+110px)] bottom-[140px] rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute -scale-x-[1] w-12 h-10 left-[calc(50%+95px)] bottom-[140px] rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "37",
     image: "/images/odontogram/i7.png",
     statusStyles: STATUS_STYLES.corona,
     styles:
-      "absolute -scale-x-[1] w-12 h-10 left-[calc(50%+120px)] bottom-[185px] rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute -scale-x-[1] w-12 h-10 left-[calc(50%+100px)] bottom-[185px] rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
   {
     label: "38",
     image: "/images/odontogram/i8.png",
     statusStyles: STATUS_STYLES.ausente,
     styles:
-      "absolute -scale-x-[1] w-12 h-10 left-[calc(50%+130px)] bottom-[230px] rounded-full hover:border hover:border-cta-100 hover:border-4",
+      "absolute -scale-x-[1] w-12 h-10 left-[calc(50%+105px)] bottom-[230px] rounded-full hover:border hover:border-cta-100 hover:border-4",
   },
 ];
 
@@ -387,7 +387,7 @@ export default function Odontogram() {
             const labelStyle = quadrant === "2" ? "-scale-x-[1]" : "";
             const toothStyle =
               tooth >= 4
-                ? "top-[-5px] left-[-30px] "
+                ? "bottom-0 left-[55px] "
                 : tooth >= 2
                 ? "top-[-25px] left-[-5px]"
                 : "top-[-30px] left-[calc(50%-10px)]";
@@ -422,7 +422,7 @@ export default function Odontogram() {
             const labelStyle = quadrant === "3" ? "-scale-x-[1]" : "";
             const toothStyle =
               tooth >= 4
-                ? "bottom-[5px] left-[-30px] "
+                ? "top-[10%] left-[65px] "
                 : tooth >= 2
                 ? "bottom-[-25px] left-[-5px]"
                 : "bottom-[-30px] left-[calc(50%-10px)]";
