de730a22958a2591ad824f2c43c61cb99025fae5
den <den.velez@gmail.com>
2024-05-13 12:01:23 -0600

    OD-26 add pwa config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
+import withPWAInit from "@ducanh2912/next-pwa";
+
 const nextConfig = {
   images: {
     domains: ["i.pravatar.cc"],
   },
 };
 
-export default nextConfig;
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: false,
+});
+
+export default withPWA(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
 
 const inter = Nunito_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  manifest: "./manifest.json",
   title: "OrthoDx",
-  description: "Welcome to orthodx, the best orthodontic app",
+  description: "Welcome to orthodx, the best orthodontic diagnotics app",
+};
+
+export const viewport: Viewport = {
+  themeColor: "#2563eb",
 };
 
 export default function RootLayout({
