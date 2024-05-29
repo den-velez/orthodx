fc1b05765dc81c24ab6fe09cbc20a477584442dc
den <den.velez@gmail.com>
2024-05-28 20:19:56 -0600

    OD-19 disable sw

--- next.config.mjs
+++ next.config.mjs
@@ -30,7 +30,7 @@ const nextConfig = {
 
 const withPWA = withPWAInit({
   dest: "public",
-  disable: process.env.NODE_ENV === "development",
+  disable: true,
 });
 
 export default withPWA(nextConfig);
