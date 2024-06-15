21ed54391b1423bd0f351af523dbc4160911df3c
den <den.velez@gmail.com>
2024-06-14 20:03:17 -0600

    OD-36 update message signup success

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -102,7 +102,7 @@ const SignUpPage = () => {
       <ModalComponent isOpen={showModal}>
         <ModalMessageComponent
           type='success'
-          message='Cuenta creada exitosamente'
+          message='Cuenta creada exitosamente, revisa tu correo para verificar tu cuenta y poder iniciar sesión.'
         />
       </ModalComponent>
       <div className='flex flex-col items-center justify-center px-3 py-3 mx-auto lg:py-0'>
@@ -231,44 +231,3 @@ const SignUpPage = () => {
 };
 
 export default SignUpPage;
-
-// import React, { useState } from 'react';
-// import { auth } from './firebase';
-
-// const SignIn = () => {
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       await auth.signInWithEmailAndPassword(email, password);
-//       // Handle successful sign-in
-//     } catch (error) {
-//       // Handle errors
-//     }
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-//       <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-//       <button type="submit">Sign In</button>
-//     </form>
-//   );
-// };
-
-// export default SignIn;
-
-// import { useAuthState } from 'react-firebase-hooks/auth';
-// import { auth } from './firebase';
-
-// const ProtectedRoute = ({ children }) => {
-//   const [user] = useAuthState(auth);
-
-//   if (!user) {
-//     return <Redirect to="/signin" />;
-//   }
-
-//   return children;
-// };
